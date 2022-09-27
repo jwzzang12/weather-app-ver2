db94ddc28417536080bdc17de441d358e34eacf8
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2022-09-26 22:16:26 -0500

    clearing extra imports and structuring the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  //const url ="https://api.openweathermap.org/data/2.5/weather?lat=32.76876910387183&lon=-97.06689230629766&appid=0f86c0f97f4a62686b4859344ba72ac1";
 
-   const ur='https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=0f86c0f97f4a62686b4859344ba72ac1'
-   
   return (
     <div className="App">
-
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>65 F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom"></div>
+      </div>
     </div>
   );
 }
