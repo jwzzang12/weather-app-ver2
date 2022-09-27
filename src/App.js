ad916fe64f465955b485a3ab9b92b23a2a47d815
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2022-09-26 20:40:51 -0500

    adding API key

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+   const ur='https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=0f86c0f97f4a62686b4859344ba72ac1'
+   
   return (
     <div className="App">
 
