94e66109c2c5ab73e2d64ae3eb5c0a778963ea94
jwzzang12 <moonbb12@gmail.com>
2022-09-28 14:20:48 +0900

    Organize code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-// import Location from "./components/Location";
 import { useState, useEffect } from "react";
 import useGeoLocation from "./hooks/useGeolocation";
 import axios from "axios";
 
 function App() {
-  //const url ="https://api.openweathermap.org/data/2.5/weather?lat=32.76876910387183&lon=-97.06689230629766&appid=0f86c0f97f4a62686b4859344ba72ac1";
   const location = useGeoLocation();
   const lat = JSON.stringify(location.coordinates.lat);
   const lon = JSON.stringify(location.coordinates.lon);
