41002f27792ac7602ccac79079e4b4c1860cbda5
jwzzang12 <moonbb12@gmail.com>
2022-09-28 13:42:22 +0900

    Install axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
-import Location from "./components/Location";
+// import Location from "./components/Location";
+import { useState, useEffect } from "react";
+import useGeoLocation from "./hooks/useGeolocation";
+import axios from "axios";
 
 function App() {
   //const url ="https://api.openweathermap.org/data/2.5/weather?lat=32.76876910387183&lon=-97.06689230629766&appid=0f86c0f97f4a62686b4859344ba72ac1";
-
+  const location = useGeoLocation();
+  const lat = JSON.stringify(location.coordinates.lat);
+  const lon = JSON.stringify(location.coordinates.lon);
+  useEffect(() => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&&appid=0f86c0f97f4a62686b4859344ba72ac1`).then((res) => {
+      console.log(res.data);
+    });
+  });
   return (
     <div className="App">
       <div className="container">
@@ -22,7 +32,6 @@ function App() {
           <div className="feels"></div>
         </div>
       </div>
-      <Location />
     </div>
   );
 }
