644017567944c7c6cf2205fa306c16100c94b0e5
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2022-09-28 20:40:07 -0500

    planing

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
   useEffect(() => {
     axios
       .get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=32.76876910387183&lon=-97.06689230629766&&appid=0f86c0f97f4a62686b4859344ba72ac1`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&&appid=0f86c0f97f4a62686b4859344ba72ac1`
       )
       .then((res) => {
-        console.log(res.data);
+        console.log(res.data); 
       });
   });
   return (
@@ -22,7 +22,7 @@ function App() {
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Dallas</p>
+            <p>{}</p>
           </div>
           <div className="temp">
             <h1>65 F</h1>
